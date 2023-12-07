2bffda5fc6eae0d6047aee3468d3733a8feaaeed
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-07 16:03:39 +0100

    [comp]-create-fig-comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`,
+  selector: "app-root",
+  template: `<h1>Welcome to {{ figTab }}!</h1>`,
 })
-export class AppComponent {
-  title = 'Ma\'Iri: Time\'s Maëlströms';
+export class AppComponent implements OnInit {
+  // title = 'Ma\'Iri: Time\'s Maëlströms';
+  figTab = ["Qalis", "Klosiums", "Pleegards"];
+
+  ngOnInit() {
+    console.table(this.figTab);
+    this.selectFig("Qualis");
+  }
+
+  selectFig(figName: string) {
+    console.log(`Vous avez selectionné une figurine de la Race des ${figName}`);
+  }
 }
