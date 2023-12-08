5285b81f3beb6537868bc316131ad2d8c4d696c9
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-08 11:26:40 +0100

    [comp]-use-eventbinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,22 @@ import { Figurine } from "./figurine";
   templateUrl: "./figurine.component.html",
 })
 export class AppComponent implements OnInit {
-  title = "Ma\'Iri";
+  title = "Ma'Iri";
   figTab: Figurine[] = FIGURINES; // Type as a Table of Figurine
   // figTab = ["Qalis", "Klosiums", "Pleegards"];
 
   ngOnInit() {
     console.table(this.figTab);
-    this.selectFig(this.figTab[0]); // Select index into the figTab Variable
+    // this.selectFig(this.figTab[0]); // Select index into the figTab Variable
   }
 
-  selectFig(figurine: Figurine) {
-    // Type as a Object "Figurine"
-    console.log(`Vous avez selectionné une figurine de la Race des ${figurine.name}`); // Call name property from Object Figurine
+  // selectFig(figurine: Figurine) {
+  //   // Type as a Object "Figurine"
+  //   console.log(`Vous avez selectionné une figurine de la Race des ${figurine.name}`); // Call name property from Object Figurine
+  // }
+
+  selectFig(event: MouseEvent) {
+    const index: number = +(event.target as HTMLInputElement).value; // Cast Event Type as HTML Type and use JS Méthodes +, targer, value to intercept DOM events and change it on an number
+    console.log(`Vous avez selectionné une figurine de la Race des ${this.figTab[index].name}`); // Call index selected by User
   }
 }
