e43181aed872e3c71aec4cf7429709bb8e432edb
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-08 17:18:54 +0100

    [comp]-logic-figurinecomp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,25 @@ import { Figurine } from "./figurine";
   selector: "app-root",
   templateUrl: "./figurine.component.html",
 })
+
 export class AppComponent implements OnInit {
   title = "Ma'Iri";
   figTab: Figurine[] = FIGURINES; // Type as a Table of Figurine
-  // figTab = ["Qalis", "Klosiums", "Pleegards"];
+  figSelected: Figurine | undefined;
 
   ngOnInit() {
     console.table(this.figTab);
-    // this.selectFig(this.figTab[0]); // Select index into the figTab Variable
   }
 
-  // selectFig(figurine: Figurine) {
-  //   // Type as a Object "Figurine"
-  //   console.log(`Vous avez selectionné une figurine de la Race des ${figurine.name}`); // Call name property from Object Figurine
-  // }
-
-  selectFig(event: MouseEvent) {
-    const index: number = +(event.target as HTMLInputElement).value; // Cast Event Type as HTML Type and use JS Méthodes +, targer, value to intercept DOM events and change it on an number
-    console.log(`Vous avez selectionné une figurine de la Race des ${this.figTab[index].name}`); // Call index selected by User
+  selectFig(figId: string) {
+    const id = +figId;
+    const figurine: Figurine | undefined = this.figTab.find((figurine) => figurine.id == +figId);
+    if (figurine) {
+      console.log(`Vous avez selectionné la figurine ${figurine.name}`);
+      this.figSelected = figurine;
+    } else {
+      console.log(`Vous avez demandé une figurine qui n'existe pas...`);
+      this.figSelected = figurine;
+    }
   }
 }
