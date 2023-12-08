ff8c4b7f08804e1b46c7ac3ac7f262cc74e93127
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-08 22:57:49 +0100

    [dir]-custom-style-by-method

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from "@angular/core";
 
 @Directive({
-  selector: '[appBorderCard]'
+  selector: "[figBorderCard]",  // modify "appBorderCard" name with your own name
 })
+
 export class BorderCardDirective {
+  constructor(private el: ElementRef) {  // DOM reference where the Directive push
+    this.setHeight(180);
+    this.setBorder("#f5f5f5");
+  }
 
-  constructor() { }
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;  // Modify style by Method
+  }
 
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  }
 }
