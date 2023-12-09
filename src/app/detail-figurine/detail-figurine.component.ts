9bc517205292cfe0c9f47a5c20fa5589da3c3437
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-09 21:53:51 +0100

    [route]-add-dynamic-routes

--- src/app/detail-figurine/detail-figurine.component.ts
+++ src/app/detail-figurine/detail-figurine.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router"; // Give access to the current Route
+import { FIGURINES } from "../mock-figurine-list";
+import { Figurine } from "../figurine";
 
 @Component({
-  selector: 'app-detail-figurine',
-  templateUrl: './detail-figurine.component.html',
-  styles: [
-  ]
+  selector: "app-detail-figurine",
+  templateUrl: "./detail-figurine.component.html",
+  styles: [],
 })
+
 export class DetailFigurineComponent {
+  figurineList: Figurine[];
+  figurine: Figurine | undefined;
+
+  constructor(private route: ActivatedRoute) {} // Dependence Injection allow the Route Service disable into the Component
+
+  ngOnInit() {
+    this.figurineList = FIGURINES;
+    const figurineId: string | null = this.route.snapshot.paramMap.get("id"); // Call the Route and take all parameters in a specific time, then get id in the URL
+    if (figurineId) {  // Condition: if a figurine was found, the id will put on this figurine
+      this.figurine = this.figurineList.find((figurine) => figurine.id == +figurineId);
+    }
+  }
+
+  goToFigurineList() {
+
+  }
 
 }
