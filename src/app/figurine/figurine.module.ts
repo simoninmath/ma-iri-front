95a177f756b74f13daa10795a4ef9c7f8a3f6ff4
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-11 11:14:34 +0100

    [module]-update-declarations-figurinemodulets

--- src/app/figurine/figurine.module.ts
+++ src/app/figurine/figurine.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ListFigurineComponent } from './list-figurine/list-figurine.component';
+import { DetailFigurineComponent } from './detail-figurine/detail-figurine.component';
+import { BorderCardDirective } from './border-card.directive';
+import { FigurineTypeColorPipe } from './figurine-type-color.pipe';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListFigurineComponent,
+    DetailFigurineComponent,
+    BorderCardDirective,
+    FigurineTypeColorPipe
+  ],
   imports: [
     CommonModule
   ]
 })
+
 export class FigurineModule { }
