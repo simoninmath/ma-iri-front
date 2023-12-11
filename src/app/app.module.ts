8bcf47f05b34c96fe0b77127a559043da7b12640
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-11 11:38:22 +0100

    [module]-clean-appmodulets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { FigurineTypeColorPipe } from './figurine/figurine-type-color.pipe';
-import { ListFigurineComponent } from './list-figurine/list-figurine.component';
-import { DetailFigurineComponent } from './detail-figurine/detail-figurine.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    FigurineTypeColorPipe,
-    ListFigurineComponent,
-    DetailFigurineComponent,
     PageNotFoundComponent
   ],
   imports: [
@@ -25,4 +17,5 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
