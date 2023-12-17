63c8c7d7760d6226d54229f4fb9bbcb97311c1c4
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-17 20:01:56 +0100

    [refacto]-figurine-service-ts

--- src/app/figurine/figurine.service.ts
+++ src/app/figurine/figurine.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { Figurine } from "./figurine";
-import { FIGURINES } from "./mock-figurine-list";
 import { HttpClient } from "@angular/common/http";
 import { Observable, catchError, of, tap } from "rxjs";
 
@@ -12,32 +11,56 @@ export class FigurineService {
     private http: HttpClient
   ){}
 
+  getFigurineList(): Observable<Figurine[]> {  // Return an Observable because the Object Figurine[] will be receive later
+    return this.http.get<Figurine[]>('api/figurine').pipe(  // Making a HTTP Request that return an Object Observable with URL
+      tap((figurineList) => console.table(figurineList)),  // Log response
+      catchError((error) => this.handleError(error, []))
+    ); 
+  }
+
   // getFigurineList(): Figurine[] {  // Return Type is a table of figurine named FIGURINES
   //   return FIGURINES;
   // }
 
-  getFigurineList(): Observable<Figurine[]> {  // Return an Observable because the Object Figurine[] will be receive later
-    return this.http.get<Figurine[]>('api/figurine').pipe(  // Making a HTTP Request that return an Object Observable with URL
-      tap((figurineList) => console.table(figurineList)),  // Log response
-      catchError((error) => { 
-        console.log(error);  // Log error
-        return of([]);  // Return an empty Table to avoid crash
-      })
-    ); 
+  // getFigurineList(): Observable<Figurine[]> {  // Return an Observable because the Object Figurine[] will be receive later
+  //   return this.http.get<Figurine[]>('api/figurine').pipe(  // Making a HTTP Request that return an Object Observable with URL
+  //     tap((figurineList) => console.table(figurineList)),  // Log response
+  //     catchError((error) => { 
+  //       console.log(error);  // Log error
+  //       return of([]);  // Return an empty Table to avoid crash
+  //     })
+  //   ); 
+  // }
+
+  getFigurineById(figurineId: number): Observable<Figurine | undefined> {
+    return this.http.get<Figurine>(`api/figurine/${figurineId}`).pipe(  // Making a HTTP Request that return an Object Observable with URL
+    tap((response) => this.log(response)),
+    catchError((error) => this.handleError(error, undefined))
+    );
   }
 
   // getFigurineById(figurineId: number): Figurine | undefined {
   //   return FIGURINES.find(figurine => figurine.id == figurineId);  // Look for the figurine with the specified id, from the table FIGURINES
   // }
 
-  getFigurineById(figurineId: number): Observable<Figurine | undefined> {
-    return this.http.get<Figurine>(`api/figurine/${figurineId}`).pipe(  // Making a HTTP Request that return an Object Observable with URL
-    tap((figurine) => console.log(figurine)),  // Log response
-    catchError((error) => { 
-      console.log(error);  // Log error
-      return of(undefined);  // Return undefined to avoid crash
-    })
-    );
+  // getFigurineById(figurineId: number): Observable<Figurine | undefined> {
+  //   return this.http.get<Figurine>(`api/figurine/${figurineId}`).pipe(  // Making a HTTP Request that return an Object Observable with URL
+  //   tap((response) => this.log(response)),
+  //   catchError((error) => { 
+  //     console.log(error);  // Log error
+  //     return of(undefined);  // Return undefined to avoid crash
+  //   })
+  //   );
+  // }
+
+  // Refactoring
+  private log(response: Figurine[] | Figurine | undefined){
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any){
+    console.error(error);
+    return of(errorValue); 
   }
 
   getFigurineTypeList(): string[] {
