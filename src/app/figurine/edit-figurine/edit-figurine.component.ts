f72aaed94dfc7f034df246922637afea372300ff
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-17 12:04:25 +0100

    [comp]-create-editfigurine-component

---
+++ src/app/figurine/edit-figurine/edit-figurine.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-figurine',
+  template: `
+    <p>
+      edit-figurine works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EditFigurineComponent {
+
+}
