f33eb296322c62b508bd1a80979ef3618b6919c6
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-17 12:31:57 +0100

    [comp]-implement-logic-from-edit-figurine-component

--- src/app/figurine/edit-figurine/edit-figurine.component.ts
+++ src/app/figurine/edit-figurine/edit-figurine.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Figurine } from '../figurine';
+import { FigurineService } from '../figurine.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-figurine',
   template: `
-    <p>
-      edit-figurine works!
+    <h2> {{ figurine?.name }} </h2>
+    <p *ngIf="figurine" class="center">
+      <img [src]="figurine.picture"/>
     </p>
+    <!-- use ngIf and property binding to bond @Input form figurine-form Component -->
+    <app-figurine-form *ngIf="figurine" [figurine]="figurine"></app-figurine-form>
   `,
   styles: [
   ]
 })
-export class EditFigurineComponent {
+export class EditFigurineComponent implements OnInit {
+
+  figurine: Figurine | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private figurineService: FigurineService
+  ){}
+  // Get the URL and figurine id, if it's right, return the selected figurine, else undefined
+  ngOnInit() {
+    const figurineId: string | null = this.route.snapshot.paramMap.get('id');
+  
+    if(figurineId) {
+      this.figurine = this.figurineService.getFigurineById(+figurineId);
+    } else {
+      this.figurine = undefined;
+    }
+
+  }
 
 }
