133b4a8b3ebfc2c344a6912c662fac373d473f8e
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-18 15:38:53 +0100

    [comp]-create-addfigurine-component

---
+++ src/app/figurine/add-figurine/add-figurine.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-figurine',
+  template: `
+    <p>
+      add-figurine works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AddFigurineComponent {
+
+}
