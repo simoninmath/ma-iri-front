3ca9a32a199e1258543999cbcb94bac9ff3159e0
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-19 18:57:47 +0100

    [refact]-detail-figurine-component

--- src/app/figurine/detail-figurine/detail-figurine.component.ts
+++ src/app/figurine/detail-figurine/detail-figurine.component.ts
@@ -31,7 +31,6 @@ export class DetailFigurineComponent {
   });
  }
 }
-
   // ngOnInit() {
   //   const figurineId: string | null = this.route.snapshot.paramMap.get("id");
   //   if (figurineId) {  
